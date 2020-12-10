2a439823fb400aad680a4f5c86df55eecac87094
oshimax <50955473+oshimax@users.noreply.github.com>
2020-12-10 16:39:34 +0900

    ES6, React, Vue

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,10 @@ const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
 
 module.exports = {
+  mode: 'development',
   devtool: 'source-map',
   entry: './src/javascripts/main.js',
   output: {
@@ -13,6 +15,15 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.vue/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'vue-loader',
+          },
+        ],
+      },
+      {
         test: /\.js/,
         exclude: /node_modules/,
         use: [
@@ -21,6 +32,7 @@ module.exports = {
             options: {
               presets: [
                 ['@babel/preset-env', { 'targets': '> 0.25%, not dead' }],
+                '@babel/preset-react',
               ],
             },
           },
@@ -34,6 +46,9 @@ module.exports = {
           },
           {
             loader: 'css-loader',
+            options: {
+              sourceMap: true,
+            },
           },
           {
             loader: 'sass-loader'
@@ -41,13 +56,22 @@ module.exports = {
         ],
       },
       {
-        test: /\.(png|jpg)/,
+        test: /\.(png|jpg|jpeg)/,
         use: [
           {
             loader: 'file-loader',
             options: {
               esModule: false,
               name: 'images/[name].[ext]'
+            },
+          },
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              mozjpeg: {
+                progressive: true,
+                quality: 65,
+              }
             },
           },
         ],
@@ -69,6 +93,7 @@ module.exports = {
     ],
   },
   plugins: [
+    new VueLoaderPlugin(),
     new MiniCssExtractPlugin({
       filename: './stylesheets/main.css',
     }),
